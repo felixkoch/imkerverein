7c34a6a154c8d0e876236d208f931ed3a0e2d2f0
Felix Koch <mail@felixkoch.de>
2019-02-27 20:17:26 +0100

    react-helmet, jquery für bootstrap

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,14 +1,71 @@
 import React from "react";
-import { graphql } from 'gatsby' 
+import { graphql } from "gatsby";
+import { Helmet } from "react-helmet";
 
 const Page = ({ children, data }) => (
   <div>
-    {data.contentfulPage.title}
-    <br />
-    <div dangerouslySetInnerHTML={{__html: data.contentfulPage.body.childContentfulRichText.html}}/>;
-    
+        <Helmet>
+      <script
+        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossOrigin="anonymous"
+      />
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossOrigin="anonymous"
+      />
+      <script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossOrigin="anonymous"
+      />
+    </Helmet>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+      🐝 Freizeitimker Wümme-Region e.V.
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+    <div>
+      {data.contentfulPage.title}
+      <br />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: data.contentfulPage.body.childContentfulRichText.html
+        }}
+      />
+      ;
+    </div>
+
   </div>
-)
+);
 
 export default Page;
 
