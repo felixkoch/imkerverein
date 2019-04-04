8b2f859da292c3743f2e9c88021f9e3ae6e5c3f7
Felix Koch <mail@felixkoch.de>
2019-04-04 20:36:50 +0200

    polyfill ie11

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Layout from "../components/Layout";
-import SEO from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout title="404: Seite nicht gefunden">
